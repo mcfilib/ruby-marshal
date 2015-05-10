68dda03bbc07d70d91a3707b252835ce0435c7d2
Philip Cunningham <philip@irisconnect.co.uk>
2015-05-10 12:23:59 +0100

    add support for parsing hashes.
    closes https://github.com/filib/ruby-marshal/issues/2

--- src/Data/Ruby/Marshal/Object.hs
+++ src/Data/Ruby/Marshal/Object.hs
@@ -29,4 +29,5 @@ getRubyObject = do
      | c == 70 || c == 84 -> RBool   <$> getBool
      | c == 105           -> RFixnum <$> getFixnum
      | c == 91            -> RArray  <$> getArray getRubyObject
+     | c == 123           -> RHash   <$> getHash getRubyObject getRubyObject
      | otherwise          -> return   $  RError Unsupported
