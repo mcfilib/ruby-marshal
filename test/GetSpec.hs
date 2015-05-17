62173ad92859d10e72e15fbd39174a571518a46d
unsymbol <hello@philipcunningham.org>
2015-05-17 19:42:12 +0100

    add some additional specs.

--- test/GetSpec.hs
+++ test/GetSpec.hs
@@ -12,8 +12,22 @@ import qualified Data.ByteString as BS
 nil :: BS.ByteString
 nil = BS.pack [48]
 
+true :: BS.ByteString
+true = BS.pack [84]
+
+false :: BS.ByteString
+false = BS.pack [70]
+
 spec :: Spec
-spec = describe "getNil" $
+spec = describe "getNil" $ do
   context "when we have nil" $
-    it "should parse" $
+    it "should parse" $ do
       runGet getNil nil `shouldBe` Right ()
+
+  context "when we have true" $
+    it "should parse" $ do
+      runGet getBool true `shouldBe` Right True
+
+  context "when we have false" $
+    it "should parse" $ do
+      runGet getBool false `shouldBe` Right False
