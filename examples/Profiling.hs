5ea390a9262d8ae5bc2b484e1a86bafd388d002f
Philip Cunningham <philip@irisconnect.co.uk>
2015-06-01 19:55:23 +0100

    go back to array example.

--- examples/Profiling.hs
+++ examples/Profiling.hs
@@ -5,14 +5,22 @@ module Main where
 import Data.Monoid      (mconcat)
 import System.Directory (getCurrentDirectory)
 
+import Data.Foldable     (foldr')
 import Data.Ruby.Marshal (load, RubyObject(..))
+import Data.Vector       (Vector)
 
 import qualified Data.ByteString as BS
+
+sumFixnum :: Vector RubyObject -> Integer
+sumFixnum xs = foldr' (+) 0 $ fmap f xs
+  where
+    f :: RubyObject -> Integer
+    f (RFixnum x) = toInteger x
 
 main :: IO ()
 main = do
   dir <- getCurrentDirectory
-  rbs <- BS.readFile (mconcat [dir, "/test/bin/42.bin"])
+  rbs <- BS.readFile (mconcat [dir, "/test/bin/bigArray.bin"])
   putStrLn . show $ load rbs >>= \case
-    RFixnum x -> Just (x * x)
+    RArray xs -> Just $ sumFixnum xs
     _         -> Nothing
