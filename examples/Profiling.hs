d8a456841b8037e41e64d5c65f98d2d71c3d5b32
Philip Cunningham <philip@irisconnect.co.uk>
2015-06-01 19:52:33 +0100

    add first example.

--- examples/Profiling.hs
+++ examples/Profiling.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import Data.Monoid      (mconcat)
 import System.Directory (getCurrentDirectory)
 
+import Data.Ruby.Marshal           as Marshal
 import qualified Data.ByteString   as BS
-import qualified Data.Ruby.Marshal as Marshal
 
 main :: IO ()
 main = do
   dir <- getCurrentDirectory
   rbs <- BS.readFile (mconcat [dir, "/test/bin/42.bin"])
-  putStrLn . show $ Marshal.load rbs
+  putStrLn . show $ Marshal.load rbs >>= \case
+    RFixnum x -> Just (x * x)
+    _         -> Nothing
