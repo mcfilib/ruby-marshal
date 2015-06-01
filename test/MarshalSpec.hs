765e8f89121bf85482cb0f14324e5a9a568dcdef
Philip Cunningham <philip@irisconnect.co.uk>
2015-06-01 19:25:26 +0100

    replace either w/just.

--- test/MarshalSpec.hs
+++ test/MarshalSpec.hs
@@ -8,7 +8,7 @@ import Test.Hspec
 import qualified Data.ByteString as BS
 import qualified Data.Vector     as V
 
-loadBin :: FilePath -> IO (Either String RubyObject)
+loadBin :: FilePath -> IO (Maybe RubyObject)
 loadBin path = do
     bs <- BS.readFile path
     return $ load bs
@@ -18,74 +18,74 @@ spec = describe "load" $ do
   context "when we have nil" $
     it "should parse" $ do
       object <- loadBin "test/bin/nil.bin"
-      object `shouldBe` Right RNil
+      object `shouldBe` Just RNil
 
   context "when we have true" $
     it "should parse" $ do
       object <- loadBin "test/bin/true.bin"
-      object `shouldBe` Right (RBool True)
+      object `shouldBe` Just (RBool True)
 
   context "when we have false" $
     it "should parse" $ do
       object <- loadBin "test/bin/false.bin"
-      object `shouldBe` Right (RBool False)
+      object `shouldBe` Just (RBool False)
 
   context "when we have 0" $
     it "should parse" $ do
       object <- loadBin "test/bin/0.bin"
-      object `shouldBe` Right (RFixnum 0)
+      object `shouldBe` Just (RFixnum 0)
 
   context "when we have -42" $
     it "should parse" $ do
       object <- loadBin "test/bin/neg42.bin"
-      object `shouldBe` Right (RFixnum (-42))
+      object `shouldBe` Just (RFixnum (-42))
 
   context "when we have 42" $
     it "should parse" $ do
       object <- loadBin "test/bin/42.bin"
-      object `shouldBe` Right (RFixnum 42)
+      object `shouldBe` Just (RFixnum 42)
 
   context "when we have -2048" $
     it "should parse" $ do
       object <- loadBin "test/bin/neg2048.bin"
-      object `shouldBe` Right (RFixnum (-2048))
+      object `shouldBe` Just (RFixnum (-2048))
 
   context "when we have 2048" $
     it "should parse" $ do
       object <- loadBin "test/bin/2048.bin"
-      object `shouldBe` Right (RFixnum 2048)
+      object `shouldBe` Just (RFixnum 2048)
 
   context "when we have [nil]" $
     it "should parse" $ do
       object <- loadBin "test/bin/nilArray.bin"
-      object `shouldBe` Right (RArray $ V.fromList [RNil])
+      object `shouldBe` Just (RArray $ V.fromList [RNil])
 
   context "when we have [true, false]" $
     it "should parse" $ do
       object <- loadBin "test/bin/boolArray.bin"
-      object `shouldBe` Right (RArray $ V.fromList [RBool True, RBool False])
+      object `shouldBe` Just (RArray $ V.fromList [RBool True, RBool False])
 
   context "when we have [-2048, -42, 0, 42, 2048]" $
     it "should parse" $ do
       object <- loadBin "test/bin/fixnumArray.bin"
-      object `shouldBe` Right (RArray $ V.fromList [RFixnum (-2048), RFixnum (-42), RFixnum 0, RFixnum 42, RFixnum 2048])
+      object `shouldBe` Just (RArray $ V.fromList [RFixnum (-2048), RFixnum (-42), RFixnum 0, RFixnum 42, RFixnum 2048])
 
   context "when we have ['hello', 'haskell']" $
     it "should parse" $ do
       object <- loadBin "test/bin/stringArray.bin"
-      object `shouldBe` Right (RArray $ V.fromList [RString "hello", RString "haskell"])
+      object `shouldBe` Just (RArray $ V.fromList [RString "hello", RString "haskell"])
 
   context "when we have { 0 => false, 1 => true }" $
     it "should parse" $ do
       object <- loadBin "test/bin/fixnumHash.bin"
-      object `shouldBe` Right (RHash $ V.fromList [(RFixnum 0, RBool False), (RFixnum 1, RBool True)])
+      object `shouldBe` Just (RHash $ V.fromList [(RFixnum 0, RBool False), (RFixnum 1, RBool True)])
 
   context "when we have 'hello haskell'" $
     it "should parse" $ do
       object <- loadBin "test/bin/rawString.bin"
-      object `shouldBe` Right (RString "hello haskell")
+      object `shouldBe` Just (RString "hello haskell")
 
   context "when we have 3.33333" $
     it "should parse" $ do
       object <- loadBin "test/bin/float.bin"
-      object `shouldBe` Right (RFloat 3.33333)
+      object `shouldBe` Just (RFloat 3.33333)
