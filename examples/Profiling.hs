5ed9c205891cd203e751f304b92f2e7ea39fa31b
Philip Cunningham <philip@irisconnect.co.uk>
2015-06-01 19:53:16 +0100

    tidy up example.

--- examples/Profiling.hs
+++ examples/Profiling.hs
@@ -5,13 +5,14 @@ module Main where
 import Data.Monoid      (mconcat)
 import System.Directory (getCurrentDirectory)
 
-import Data.Ruby.Marshal           as Marshal
-import qualified Data.ByteString   as BS
+import Data.Ruby.Marshal (load, RubyObject(..))
+
+import qualified Data.ByteString as BS
 
 main :: IO ()
 main = do
   dir <- getCurrentDirectory
   rbs <- BS.readFile (mconcat [dir, "/test/bin/42.bin"])
-  putStrLn . show $ Marshal.load rbs >>= \case
+  putStrLn . show $ load rbs >>= \case
     RFixnum x -> Just (x * x)
     _         -> Nothing
