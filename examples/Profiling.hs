74b2a53ee4cbaa0c73f07a6a15c7df08dea475c0
Philip Cunningham <philip@irisconnect.co.uk>
2015-06-02 17:41:31 +0100

    remove debugging.

--- examples/Profiling.hs
+++ examples/Profiling.hs
@@ -7,29 +7,20 @@ import System.Directory (getCurrentDirectory)
 
 import Data.Ruby.Marshal (load, RubyObject(..))
 import Data.Vector       (Vector)
-import Debug.Trace       (traceShow)
 
 import qualified Data.ByteString as BS
 import qualified Data.Foldable   as F
 
-debug :: Show a => a -> a
-debug x = traceShow x x
-
-loadBigArray :: IO (Maybe RubyObject)
-loadBigArray = do
-  dir <- getCurrentDirectory
-  rbs <- BS.readFile (mconcat [dir, "/test/bin/bigArray.bin"])
-  return $ load rbs
-
 sumFixnum :: Vector RubyObject -> Integer
-sumFixnum xs = F.foldr' (+) 0 $ (debug (fmap f xs))
+sumFixnum xs = F.foldl' (+) 0 $ fmap f xs
   where
     f :: RubyObject -> Integer
     f (RFixnum x) = toInteger x
 
 main :: IO ()
 main = do
-  array <- loadBigArray
-  print $ case array of
-    Just (RArray xs) -> Just $ sumFixnum xs
-    _                -> Nothing
+  dir <- getCurrentDirectory
+  rbs <- BS.readFile (mconcat [dir, "/test/bin/bigArray.bin"])
+  print $ load rbs >>= \case
+    RArray xs -> Just $ sumFixnum xs
+    _         -> Nothing
