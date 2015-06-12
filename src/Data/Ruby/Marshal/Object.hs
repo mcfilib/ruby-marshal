7c6687835464c9e7e693c15eeb3f9b466e986576
Philip Cunningham <philip@irisconnect.co.uk>
2015-06-12 08:20:19 +0100

    extracted types.hs.

--- src/Data/Ruby/Marshal/Object.hs
+++ src/Data/Ruby/Marshal/Object.hs
@@ -22,6 +22,7 @@ module Data.Ruby.Marshal.Object (
 
 import Control.Applicative
 import Data.Ruby.Marshal.Get
+import Data.Ruby.Marshal.Types
 import Prelude
 
 import Data.Serialize     (Serialize(..))
@@ -29,46 +30,6 @@ import Data.Serialize.Get (Get, getWord8)
 import Data.Vector        (Vector)
 
 import qualified Data.ByteString as BS
-
-data Error
-  = Unknown
-    -- ^ represents an unknown Ruby object
-  | Unsupported
-    -- ^ represents an unsupported Ruby object
-  deriving (Eq, Show)
-
-data RubyObject
-  = RNil
-    -- ^ represents @nil@
-  | RBool                  !Bool
-    -- ^ represents @true@ or @false@
-  | RFixnum {-# UNPACK #-} !Int
-    -- ^ represents a @Fixnum@
-  | RArray                 !(Vector RubyObject)
-    -- ^ represents an @Array@
-  | RHash                  !(Vector (RubyObject, RubyObject))
-    -- ^ represents an @Hash@
-  | RString                !BS.ByteString
-    -- ^ represents a @String@
-  | RFloat {-# UNPACK #-}  !Double
-    -- ^ represents a @Float@
-  | RSymbol                !BS.ByteString
-    -- ^ represents a @Symbol@
-  | RError                 !Error
-    -- ^ represents an invalid object
-  deriving (Eq, Show)
-
--- | Allow easy pattern matching of values.
-pattern NilC    = 48
-pattern TrueC   = 84
-pattern FalseC  = 70
-pattern FixnumC = 105
-pattern ArrayC  = 91
-pattern HashC   = 123
-pattern IvarC   = 73
-pattern StringC = 34
-pattern FloatC  = 102
-pattern SymbolC = 58
 
 -- | Parses a subset of Ruby objects.
 getRubyObject :: Get RubyObject
