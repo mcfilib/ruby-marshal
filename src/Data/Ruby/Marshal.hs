9449e822dbf4ae1804d87b8edd84fbf484b4359e
Philip Cunningham <philip@irisconnect.co.uk>
2015-07-26 18:28:04 +0100

    add decodeEither function.
    from dukerutledge:
    
    Is there a specific reason you are converting your Either into a Maybe?
    I understand Maybes are more ubiquitous, but that Either could contain
    valuable information for the consumer. At least expose a decodeEither
    function.
    
    my reply:
    
    My motivation was to keep the interface small for consumers but I think
    you're absolutely right that using Either will be more informative than
    Maybe. I'll go ahead and write that decodeEither function. Thanks!

--- src/Data/Ruby/Marshal.hs
+++ src/Data/Ruby/Marshal.hs
@@ -14,10 +14,11 @@
 
 module Data.Ruby.Marshal (
   -- * Simple interface to deserialise Ruby Marshal binary
-  decode,
+    decode
+  , decodeEither
   -- * Re-exported modules
-  module Data.Ruby.Marshal.Get,
-  module Data.Ruby.Marshal.Types
+  , module Data.Ruby.Marshal.Get
+  , module Data.Ruby.Marshal.Types
 ) where
 
 import Data.Ruby.Marshal.Get
@@ -29,6 +30,22 @@ import Data.Serialize      (runGet)
 import qualified Data.ByteString as BS
 import qualified Data.Vector     as V
 
+-- | Deserialises a subset of Ruby objects serialised with Marshal, Ruby's
+-- built-in binary serialisation format.
+decode :: BS.ByteString
+       -- ^ Serialised Ruby object
+       -> Maybe RubyObject
+       -- ^ De-serialisation result
+decode = hush . decodeEither
+
+-- | Deserialises a subset of Ruby objects serialised with Marshal, Ruby's
+-- built-in binary serialisation format.
+decodeEither :: BS.ByteString
+             -- ^ Serialised Ruby object
+             -> Either String RubyObject
+             -- ^ Error message or de-serialisation result
+decodeEither = runGet (evalStateT (runMarshal getRubyObject) emptyCache)
+
 -- | Constructs an empty cache to store symbols and objects.
 emptyCache :: Cache
 emptyCache = Cache { _symbols = V.empty, _objects = V.empty }
@@ -36,12 +53,3 @@ emptyCache = Cache { _symbols = V.empty, _objects = V.empty }
 -- | Converts an Either to a Maybe.
 hush :: Either a b -> Maybe b
 hush = either (const Nothing) Just
-
--- | Deserialises a subset of Ruby objects serialised with Marshal, Ruby's
--- built-in binary serialisation format.
-decode :: BS.ByteString
-     -- ^ Serialised Ruby object
-     -> Maybe RubyObject
-     -- ^ De-serialisation result
-decode x = hush $
-  runGet (evalStateT (runMarshal getRubyObject) emptyCache) x
