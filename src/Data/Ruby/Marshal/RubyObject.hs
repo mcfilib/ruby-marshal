2dd35078a75d023a2127ec6c84dc82c19ad55322
Philip Cunningham <hello@filib.io>
2015-08-01 10:53:06 +0100

    replace rubyable with ruby.

--- src/Data/Ruby/Marshal/RubyObject.hs
+++ src/Data/Ruby/Marshal/RubyObject.hs
@@ -65,7 +65,7 @@ data Error
   deriving (Eq, Ord, Show)
 
 -- | Transform plain Haskell values to RubyObjects and back.
-class Rubyable a where
+class Ruby a where
   -- | Takes a plain Haskell value and lifts into RubyObject
   toRuby :: a -> RubyObject
   -- | Takes a RubyObject transforms it into a more general Haskell value.
@@ -73,53 +73,53 @@ class Rubyable a where
 
 -- core instances
 
-instance Rubyable RubyObject where
+instance Ruby RubyObject where
   toRuby = id
   fromRuby = Just
 
-instance Rubyable () where
+instance Ruby () where
   toRuby _ = RNil
   fromRuby = \case
     RNil -> Just ()
     _    -> Nothing
 
-instance Rubyable Bool where
+instance Ruby Bool where
   toRuby = RBool
   fromRuby = \case
     RBool x -> Just x
     _       -> Nothing
 
-instance Rubyable Int where
+instance Ruby Int where
   toRuby = RFixnum
   fromRuby = \case
     RFixnum x -> Just x
     _         -> Nothing
 
-instance Rubyable a => Rubyable (V.Vector a) where
+instance Ruby a => Ruby (V.Vector a) where
   toRuby = RArray . V.map toRuby
   fromRuby = \case
     RArray x -> V.mapM fromRuby x
     _        -> Nothing
 
-instance (Rubyable a, Rubyable b) => Rubyable (V.Vector (a, b)) where
+instance (Ruby a, Ruby b) => Ruby (V.Vector (a, b)) where
   toRuby x = RHash $ V.map (toRuby *** toRuby) x
   fromRuby = \case
     RHash x -> V.mapM (\(k, v) -> (,) <$> fromRuby k <*> fromRuby v) x
     _       -> Nothing
 
-instance Rubyable BS.ByteString where
+instance Ruby BS.ByteString where
   toRuby = RSymbol
   fromRuby = \case
     RSymbol x -> Just x
     _         -> Nothing
 
-instance Rubyable Float where
+instance Ruby Float where
   toRuby = RFloat
   fromRuby = \case
     RFloat  x -> Just x
     _         -> Nothing
 
-instance Rubyable (BS.ByteString, REncoding) where
+instance Ruby (BS.ByteString, REncoding) where
   toRuby (x, y) = RIVar (RString x, y)
   fromRuby = \case
     RIVar (RString x, y) -> Just (x, y)
@@ -127,7 +127,7 @@ instance Rubyable (BS.ByteString, REncoding) where
 
 -- nil like
 
-instance Rubyable a => Rubyable (Maybe a) where
+instance Ruby a => Ruby (Maybe a) where
   toRuby = \case
     Just x  -> toRuby x
     Nothing -> RNil
@@ -138,16 +138,16 @@ instance Rubyable a => Rubyable (Maybe a) where
 
 -- array like
 
-instance Rubyable a => Rubyable [a] where
+instance Ruby a => Ruby [a] where
   toRuby = toRuby . V.fromList
   fromRuby x = V.toList <$> fromRuby x
 
 -- map like
 
-instance (Rubyable a, Rubyable b) => Rubyable [(a, b)] where
+instance (Ruby a, Ruby b) => Ruby [(a, b)] where
   toRuby = toRuby . V.fromList
   fromRuby x = V.toList <$> fromRuby x
 
-instance (Rubyable a, Rubyable b, Ord a) => Rubyable (DM.Map a b) where
+instance (Ruby a, Ruby b, Ord a) => Ruby (DM.Map a b) where
   toRuby = toRuby . DM.toList
   fromRuby x = DM.fromList <$> fromRuby x
