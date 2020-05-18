aa280d7cdf676656a7b35e7ebbf2a2756bcecf0d
Jack Kelly <jack.kelly@bellroy.com>
2020-05-18 14:59:43 +1000

    Ruby.Marshal.Types: Add signatures to pattern synonyms
    Silences warnings.

--- src/Data/Ruby/Marshal/Types.hs
+++ src/Data/Ruby/Marshal/Types.hs
@@ -43,26 +43,38 @@ import Data.Ruby.Marshal.Monad
 import Data.Ruby.Marshal.RubyObject
 
 -- | Character that represents NilCharlass.
+pattern NilChar :: (Eq a, Num a) => a
 pattern NilChar = 48
 -- | Character that represents FalseClass.
+pattern FalseChar :: (Eq a, Num a) => a
 pattern FalseChar = 70
 -- | Character that represents TrueClass.
+pattern TrueChar :: (Eq a, Num a) => a
 pattern TrueChar = 84
 -- | Character that represents Array.
+pattern ArrayChar :: (Eq a, Num a) => a
 pattern ArrayChar = 91
 -- | Character that represents Fixnum.
+pattern FixnumChar :: (Eq a, Num a) => a
 pattern FixnumChar = 105
 -- | Character that represents Float.
+pattern FloatChar :: (Eq a, Num a) => a
 pattern FloatChar = 102
 -- | Character that represents Hash.
+pattern HashChar :: (Eq a, Num a) => a
 pattern HashChar = 123
 -- | Character that represents IVar.
+pattern IVarChar :: (Eq a, Num a) => a
 pattern IVarChar = 73
 -- | Character that represents Object link.
+pattern ObjectLinkChar :: (Eq a, Num a) => a
 pattern ObjectLinkChar = 64
 -- | Character that represents String.
+pattern StringChar :: (Eq a, Num a) => a
 pattern StringChar = 34
 -- | Character that represents Symbol.
+pattern SymbolChar :: (Eq a, Num a) => a
 pattern SymbolChar = 58
 -- | Character that represents Symlink.
+pattern SymlinkChar :: (Eq a, Num a) => a
 pattern SymlinkChar = 59
